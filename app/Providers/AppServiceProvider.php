edaf7e1c4a6b7359b1d8485172fa0d46816988e9
khunemz <khunemz@gmail.com>
2016-09-25 15:35:53 +0700

    config theme finder and assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use App\View\ThemeViewFinder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app['view']->setFinder($this->app['theme.finder']);
     }
 
     /**
@@ -23,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('theme.finder', function ($app) {
+            $finder = new ThemeViewFinder($app['files'], $app['config']['view.paths']);
+            $config = $app['config']['cms.theme'];
+            $finder->setBasePath($app['path.public'].'/'.$config['folder']);
+            $finder->setActiveTheme($config['active']);
+            return $finder;
+        });
     }
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,8 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
+elixir.config.assetsDir = 'public/themes/default/assets/';
+elixir.config.publicPath = elixir.config.asstsPath;
 
 elixir(function(mix) {
-    mix.less('app.less');
+    mix.sass('/backend.scss');
 });
